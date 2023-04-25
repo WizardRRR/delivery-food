418998f2908f5eca0e55c0c5247d1b0a44937636
Royer Rutti <74883427+WizardRRR@users.noreply.github.com>
2023-04-25 16:41:48 -0500

    primerCommit

---
+++ database/migrations/2023_04_25_213442_create_platos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('platos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_plato');
+            $table->text('descripción');
+            $table->decimal('precio', 8, 2);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('platos');
+    }
+};
